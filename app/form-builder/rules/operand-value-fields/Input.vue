947f5fd3d5fc0a49785543fefe8e1d22216f82c7
William <william@Williams-MacBook-Air.local>
2025-12-28 23:00:42 -0500

    operand value field element instance type

--- app/form-builder/rules/operand-value-fields/Input.vue
+++ app/form-builder/rules/operand-value-fields/Input.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import type { ElementInstance } from '~/form-builder/elements'
+import type { elementTypes } from '~/form-builder/elements/elementTypes'
 import { Input } from '@/components/ui/input'
 
 const props = defineProps<{
   elementInstance: Extract<
     ElementInstance,
-    { type: 'single-line' | 'multi-line' }
+    { type: typeof elementTypes.SINGLE_CHOICE | typeof elementTypes.MULTI_CHOICE }
   >
 }>()
 const modelValue = defineModel<string>()
--- app/form-builder/rules/operand-value-fields/SingleSelect.vue
+++ app/form-builder/rules/operand-value-fields/SingleSelect.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import type { ElementInstance } from '~/form-builder/elements'
+import type { elementTypes } from '~/form-builder/elements/elementTypes'
 
 const props = defineProps<{
   elementInstance: Extract<
     ElementInstance,
-    { type: 'single-choice' }
+    { type: typeof elementTypes.SINGLE_CHOICE | typeof elementTypes.MULTI_CHOICE }
   >
 }>()
 const modelValue = defineModel<string>()
